b53feb99e29790a59ba972a938d9b2b91fa23fb2
Jordan Mussi <jxm1099@cs.bham.ac.uk>
2019-01-21 11:59:24 +0000

    Packet enum

--- src/com/alien8/networking/Packet.java
+++ src/com/alien8/networking/Packet.java
@@ -1,5 +1,14 @@
 package com.alien8.networking;
 
 public abstract class Packet {
-    
+
+    public static enum PacketType {
+        HELLO(0);
+        
+        private int id;
+        
+        PacketType(int id) {
+            this.id = id;
+        }
+    }
 }
--- src/com/alien8/networking/server/ServerNetwork.java
+++ src/com/alien8/networking/server/ServerNetwork.java
@@ -11,10 +11,10 @@ import com.alien8.networking.Constants;
 public class ServerNetwork extends Thread {
     private ArrayList<PlayerConnection> connections;
     private DatagramSocket socket;
-    
+
     ServerNetwork() {
         this.connections = new ArrayList<>();
-        
+
         try {
             this.socket = new DatagramSocket(Constants.SERVER_LISTENING_PORT);
             System.out.println("Server is running...");
@@ -22,22 +22,22 @@ public class ServerNetwork extends Thread {
             e.printStackTrace();
         }
     }
-    
+
     public void run() {
         while (true) {
             byte[] data = new byte[1];
             DatagramPacket packet = new DatagramPacket(data, data.length);
-            
+
             try {
                 this.socket.receive(packet);
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            
+
             this.parse(packet);
         }
     }
-    
+
     private void parse(DatagramPacket packet) {
         
     }
