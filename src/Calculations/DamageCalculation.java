a8e5f4fa0b7c85ccf9814440c9a76df4b7292b25
kxr758 <kxr758@student.bham.ac.uk>
2019-01-22 17:51:06 +0000

    Add damage mitigation

--- src/Calculations/DamageCalculation.java
+++ src/Calculations/DamageCalculation.java
@@ -19,7 +19,7 @@ public class DamageCalculation {
                         return baseDmg * 1.5f;
                 }
             case EARTH:
-                switch (victim.getState()){
+                switch (victim.getState()) {
                     case FIRE:
                         return baseDmg * 1.5f;
                     case EARTH:
@@ -30,7 +30,7 @@ public class DamageCalculation {
                         return baseDmg * 1f;
                 }
             case WATER:
-                switch (victim.getState()){
+                switch (victim.getState()) {
                     case FIRE:
                         return baseDmg * 1.5f;
                     case EARTH:
@@ -41,7 +41,7 @@ public class DamageCalculation {
                         return baseDmg * 0.5f;
                 }
             case AIR:
-                switch (victim.getState()){
+                switch (victim.getState()) {
                     case FIRE:
                         return baseDmg * 1.5f;
                     case EARTH:
@@ -55,6 +55,64 @@ public class DamageCalculation {
                 return baseDmg;
 
         }
+    }
+
+    //The amount the damaged is reduced by, due to the shields in percentage
+    public float calculateMitgation(Player attackingPlayer, Player victim) {
+        if (victim.getIsShielded()) {
+            switch (attackingPlayer.getState()) {
+                case FIRE:
+                    switch (victim.getState()) {
+                        case FIRE:
+                            return 0.5f;
+                        case EARTH:
+                            return 1.0f;
+                        case WATER:
+                            return 1.0f;
+                        case AIR:
+                            return 0.75f;
+                    }
+                case EARTH:
+                    switch (victim.getState()) {
+                        case FIRE:
+                            return 0.75f;
+                        case EARTH:
+                            return 0.5f;
+                        case WATER:
+                            return 0.95f;
+                        case AIR:
+                            return 0.95f;
+                    }
+                case WATER:
+                    switch (victim.getState()) {
+                        case FIRE:
+                            return 0.5f;
+                        case EARTH:
+                            return 0.25f;
+                        case WATER:
+                            return 0.5f;
+                        case AIR:
+                            return 1f;
+                    }
+                case AIR:
+                    switch (victim.getState()) {
+                        case FIRE:
+                            return 1.0f;
+                        case EARTH:
+                            return 0.75f;
+                        case WATER:
+                            return 0.75f;
+                        case AIR:
+                            return 0.5f;
+                    }
+                default:
+                    return 1.0f;
+            }
+        } else {
+            return
+                    1.0f;
+        }
+
     }
 
 
