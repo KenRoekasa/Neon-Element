af69927dba15c86f61656924f91fab997bf54ee1
KennyR <kennykr@hotmail.co.uk>
2019-01-22 11:21:48 +0000

    Add damage interactions

--- src/Calculations/DamageCalculation.java
+++ src/Calculations/DamageCalculation.java
@@ -1,8 +1,61 @@
 package Calculations;
 
+import Entities.Player;
+
 public class DamageCalculation {
 
 
+    public float calculateDamage(float baseDmg, Player player, Player victim) {
+        switch (player.getState()) {
+            case FIRE:
+                switch (victim.getState()) {
+                    case FIRE:
+                        return baseDmg * 1f;
+                    case EARTH:
+                        return baseDmg * 1f;
+                    case WATER:
+                        return baseDmg * 0.5f;
+                    case AIR:
+                        return baseDmg * 1.5f;
+                }
+            case EARTH:
+                switch (victim.getState()){
+                    case FIRE:
+                        return baseDmg * 1.5f;
+                    case EARTH:
+                        return baseDmg * 1f;
+                    case WATER:
+                        return baseDmg * 0.5f;
+                    case AIR:
+                        return baseDmg * 1f;
+                }
+            case WATER:
+                switch (victim.getState()){
+                    case FIRE:
+                        return baseDmg * 1.5f;
+                    case EARTH:
+                        return baseDmg * 1f;
+                    case WATER:
+                        return baseDmg * 1f;
+                    case AIR:
+                        return baseDmg * 0.5f;
+                }
+            case AIR:
+                switch (victim.getState()){
+                    case FIRE:
+                        return baseDmg * 1.5f;
+                    case EARTH:
+                        return baseDmg * 0.5f;
+                    case WATER:
+                        return baseDmg * 1f;
+                    case AIR:
+                        return baseDmg * 1f;
+                }
+            default:
+                return baseDmg;
+
+        }
+    }
 
 
 }
