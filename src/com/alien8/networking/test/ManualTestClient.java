105740d7363506aa7b21f5e715a6a52b690e9dc4
Jordan Mussi <jxm1099@cs.bham.ac.uk>
2019-01-25 16:32:59 +0000

    Modify test code

--- src/com/alien8/networking/test/ManualTestClient.java
+++ src/com/alien8/networking/test/ManualTestClient.java
@@ -1,15 +1,29 @@
 package com.alien8.networking.test;
 
 import com.alien8.networking.client.ClientNetworkDispatcher;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+import com.alien8.networking.Constants;
 import com.alien8.networking.client.ClientNetwork;
 
 public class ManualTestClient {
     public static void main(String[] args) {
-        ClientNetwork net = new ClientNetwork();
-        net.start();
-
-        ClientNetworkDispatcher dispatcher = net.getDispatcher();
-        dispatcher.sendHello();
-        while(true) {}
+        
+        DatagramSocket socket;
+        try {
+            socket = new DatagramSocket();
+            
+            String str = "hi";
+            byte[] data = str.getBytes();
+            
+            DatagramPacket packet = new DatagramPacket(data, data.length, InetAddress.getByName("192.168.43.147"), Constants.SERVER_LISTENING_PORT);
+            socket.send(packet);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/com/alien8/networking/test/ManualTestServer.java
+++ src/com/alien8/networking/test/ManualTestServer.java
@@ -1,11 +1,29 @@
 package com.alien8.networking.test;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+import com.alien8.networking.Constants;
+import com.alien8.networking.packets.Packet;
 import com.alien8.networking.server.ServerNetwork;
 
 public class ManualTestServer {
     public static void main(String[] args) {
-        ServerNetwork net = new ServerNetwork();
-        net.start();
+        DatagramSocket socket;
+        try {
+            socket = new DatagramSocket(Constants.SERVER_LISTENING_PORT);
+   
+            byte[] data = new byte[Packet.PACKET_BYTES_LENGTH];
+            DatagramPacket packet = new DatagramPacket(data, data.length);
+            
+            socket.receive(packet);
+            
+            String str = new String(data);
+            System.out.println(str);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         while(true) {}
     }
