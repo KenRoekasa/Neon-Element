4adc655a483ef70e7134e5201898b9d9694e6eaa
yxt625 <yxt625@cs.bham.ac.uk>
2019-01-28 10:25:49 +0000

    added basic enemy class to entities

---
+++ src/Entities/BasicEnemy.java
@@ -0,0 +1,65 @@
+package src.Entities;
+
+
+import java.lang.Thread;
+import java.util.Random;
+import src.Enums.*;;
+
+public class BasicEnemy extends Character {
+
+	BasicEnemyStates activeState;
+	Character[] players;
+	
+	public BasicEnemy(Character players[]) {
+		
+		activeState = BasicEnemyStates.IDLE;
+		assignRandomElement();
+		this.players = players;
+
+	}
+	
+	public void start () {
+		
+		
+		Thread t = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				
+				boolean bool = true;
+				
+				while (bool) {
+					
+					BasicEnemyFSM.nextAction(this, players);
+					
+					if (health<=0)
+						bool=false;
+				}
+			}
+			
+		});
+		
+	}
+	
+	void assignRandomElement() {
+		
+		Random r = new Random();
+		int rand = r.nextInt(4);
+		switch(rand) {
+		case 0: changeToEarth();break;
+		case 1: changeToFire(); break;
+		case 2: changeToWater(); break;
+		case 3: changeToAir(); break;
+		}
+	}
+	
+	
+	BasicEnemyStates getActiveState() {
+		return activeState;
+	}
+	
+	void setState(BasicEnemyStates s) {
+		activeState  = s ;
+	}
+	//basic enemy actions
+}
