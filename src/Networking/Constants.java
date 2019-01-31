5db4691db10cad54dc92ba66425f28dc878758b1
kxf672 <kxf672@cs.bham.ac.uk>
2019-01-31 01:43:10 +0000

    changed server address due to testing on school machine network

--- src/Networking/Constants.java
+++ src/Networking/Constants.java
@@ -1,7 +1,8 @@
 package Networking;
 
 public class Constants {
-    public static final String SERVER_ADDRESS = "172.20.10.3";
+    public static final String SERVER_ADDRESS = "127.0.0.1";
+    //172.20.10.3
     public static final int SERVER_LISTENING_PORT = 8888;
     public static final int FIVE_SECONDS = 5;
 }
