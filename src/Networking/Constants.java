bf8137502498f4cdf1eb9e42132ae6808f9e6fa1
“kxf672” <kxf672@student.bham.ac.uk>
2019-02-01 15:58:29 +0000

    adding progressing broadcatsing code

--- src/Networking/Constants.java
+++ src/Networking/Constants.java
@@ -1,7 +1,7 @@
 package Networking;
 
 public class Constants {
-    public static final String SERVER_ADDRESS = "172.20.10.3";
+    public static final String SERVER_ADDRESS = "225.4.5.6";
     public static final int SERVER_LISTENING_PORT = 8888;
     public static final int FIVE_SECONDS = 5;
 }
--- src/Networking/Server/MultiCastServerThread.java
+++ src/Networking/Server/MultiCastServerThread.java
@@ -1,80 +1,68 @@
 package Networking.Server;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
+import java.net.MulticastSocket;
 import java.net.UnknownHostException;
 import java.util.Date;
 
 import Networking.Constants;
 import Networking.Packets.Packet;
 
-public class MultiCastServerThread extends ServerNetwork{
-    protected BufferedReader in = null;
-    protected boolean moreQuotes = true;
-
+/** A server which actively broadcasts its 
+ * 		 CONNECT_BCAST, DISCONNECT_BCAST,
+        READY_STATE, LOCATION_STATE,
+        ELEMENT_STATE_BCAST, CAST_SPELL_BCAST     ((byte) 0xF5),
+        POWERUP_PICKUP_BCAST, POWERUP_STATE_BCAST  ((byte) 0xF7),
+        GAME_START_BCAST, GAME_OVER_BCAST and to a  group */
+public class MultiCastServerThread extends ServerNetwork {
+	private InetAddress groupAddress;
+	private MulticastSocket socket;
+	private String ipAdrress;
+	
+	public MultiCastServerThread() {
+		//create a s socket for broadcasting to the desired group and port
+		/*The hard-coded port number is SERVER_LISTENING_PORT (the client must have a MulticastSocket bound to this port). 
+         * The hard-coded InetAddress of the DatagramPacket is "" and is a group identifier,
+         *  (rather than the Internet address of the machine on which a single client is running). 
+         *
+         * Created in this way, the DatagramPacket is destined for all clients listening to port number 
+         * Constants.SERVER_LISTENING_PORT 
+         * who are member of the "172.20.10.3" group.
+         * 
+         * 
+         * */
+		try {
+			groupAddress = InetAddress.getByName(Constants.SERVER_ADDRESS);
+			socket = new MulticastSocket(Constants.SERVER_LISTENING_PORT);
+			} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	}
+	
 	public void run() {
-        while (this.running) {
-        	try {
-            byte[] data = new byte[Packet.PACKET_BYTES_LENGTH];
-            
-            String dString = null;
-            if (in == null)
-                dString = new Date().toString();
-            else
-                dString = getNextQuote();
-            data = dString.getBytes();
-			
-			
-			
-			in = new BufferedReader(new FileReader("one-liners.txt"));
-			
-			
-			/*The hard-coded port number is SERVER_LISTENING_PORT (the client must have a MulticastSocket bound to this port). 
-             * The hard-coded InetAddress of the DatagramPacket is "172.20.10.3" and is a group identifier,
-             *  (rather than the Internet address of the machine on which a single client is running). 
-             *
-             * Created in this way, the DatagramPacket is destined for all clients listening to port number 
-             * Constants.SERVER_LISTENING_PORT 
-             * who are member of the "172.20.10.3" group.
-             * 
-             * 
-             * */
-			InetAddress group = InetAddress.getByName("172.20.10.3");
-            DatagramPacket packet = new DatagramPacket(data, data.length, group, Constants.SERVER_LISTENING_PORT);
-          
-
-            this.socket.send(packet);
-               // this.socket.receive(packet);
-                
-                //this.parse(packet);
-                try {
-                    sleep((long)Math.random() * (Constants.FIVE_SECONDS));
-                } 
-                catch (InterruptedException e) { }	
-                
-            } catch (IOException e ) {
-                e.printStackTrace();
-            }
-
-        }
-        this.socket.close();
+       /*do some broadcasting and then it sleeps or
+        *  can wait for the next processing*/
+		
+		try {
+			sleep(500);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+       
     }
 	
-	protected String getNextQuote() {
-        String returnValue = null;
-        try {
-            if ((returnValue = in.readLine()) == null) {
-                in.close();
-		moreQuotes = false;
-                returnValue = "No more quotes. Goodbye.";
-            }
-        } catch (IOException e) {
-            returnValue = "IOException occurred in server.";
-        }
-        return returnValue;
-    }
-		
+	
+
+	
 }
