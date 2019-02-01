45709cdf94d0077aaf975ba19ca7cf45b714d5e2
Tom Wiley <tomos.g.wiley@gmail.com>
2019-02-01 16:52:36 +0000

    Refactored renderer

--- src/graphics/ISOConverter.java
+++ src/graphics/ISOConverter.java
@@ -2,6 +2,11 @@ package graphics;
 
 
 import javafx.geometry.Point2D;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.transform.Affine;
+import javafx.scene.transform.Rotate;
+
+import static javafx.scene.transform.Rotate.X_AXIS;
 
 public class ISOConverter {
 
@@ -16,6 +21,19 @@ public class ISOConverter {
         double x = point.getX()- point.getY();
         double y = (point.getX() + point.getY()) / 2;
         return new Point2D(x,y);
+    }
+
+    @SuppressWarnings("Duplicates")
+    static void applyIsoTransform(GraphicsContext gc, double xRotationCenter, double yRotationCenter) {
+        gc.save();
+        Affine affine = new Affine();
+
+        Rotate rotateX= new Rotate(45, xRotationCenter, yRotationCenter);
+        Rotate rotateZ = new Rotate(60.0, xRotationCenter, yRotationCenter, 0, X_AXIS);
+        affine.prepend(rotateX);
+        affine.prepend(rotateZ);
+
+        gc.transform(affine);
     }
 
 }
