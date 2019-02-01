2b2bd281b70aa6bb0876accd445ce04d4b774549
ysturki <ysturki@gmail.com>
2019-02-01 16:24:34 +0000

    added enemy movement methods

--- src/entities/BasicEnemy.java
+++ src/entities/BasicEnemy.java
@@ -5,11 +5,14 @@ import ai.BasicEnemyFSM;
 import ai.BasicEnemyStates;
 import entities.Character;
 import entities.PowerUp;
+import enums.PowerUpType;
+import javafx.geometry.Point2D;
+
 import java.util.Random;
 
 
 public class BasicEnemy extends Character {
-/*
+
     BasicEnemyStates activeState;
     Character [] players;
     PowerUp [] powerUps;
@@ -85,19 +88,138 @@ public class BasicEnemy extends Character {
     	//TODO: implement this and then functions for each case
     	BasicEnemyStates newState = null;
     	switch(activeState) {
-    	case ATTACK : break;
-    	case AGGRESSIVE_ATTACK: break;
-    	case FIND_HEALTH: break;
-    	case FIND_DAMAGE: break;
-    	case FIND_SPEED: break;
+    	case ATTACK :attack(); break;
+    	case AGGRESSIVE_ATTACK: aggressiveAttack(); break;
+    	case FIND_HEALTH: findHealth(); break;
+    	case FIND_DAMAGE: findDamage(); break;
+    	case FIND_SPEED: findSpeed(); break;
     	}
     }
-    //basic enemy actions
-    */
-@Override
-public void update() {
+    
+  //basic enemy actions
+    private void findSpeed() {
+    	PowerUp powerup = findNearestPowerUp( PowerUpType.SPEED);
+		moveTo(powerup);
+	}
 
-}
+	private void findDamage() {
+		PowerUp powerup = findNearestPowerUp( PowerUpType.DAMAGE);
+		moveTo(powerup);
+	}
+
+	private void findHealth() {
+		PowerUp powerup = findNearestPowerUp( PowerUpType.HEAL);
+		moveTo(powerup);
+	}
+
+	private void aggressiveAttack() {
+		Character player = findNearestPlayer();
+		moveTo(player);
+    	if(inAttackDistance(player)) {
+    		//if(canHeavyAttack() )
+    		//heavyAttack();
+    		lightAttack();
+    	}
+	}
+	
+    private void attack() {
+    	Character player = findNearestPlayer();
+    	moveTo(player);
+    	if(inAttackDistance(player)) {
+    		lightAttack();
+    	}
+    }
+    
+    private boolean inAttackDistance(Character player) {
+    	if(this.getLocation().distance(player.getLocation())<getWidth())
+    		return true;
+    	return false;
+    }
+    
+    private PowerUp findNearestPowerUp(PowerUpType pu) {
+    	Character player = findNearestPlayer();
+		int index=-1;
+		double distance = Double.MAX_VALUE;
+		for (int i = 0; i < powerUps.length; i++) {
+			if(powerUps[i].getType().equals(pu)) {
+				double disToPU = calcDistance(powerUps[i].getLocation(),getLocation());
+				if( disToPU < calcDistance(getLocation(),player.getLocation()) ) {
+					if(disToPU < distance) {
+						distance = disToPU;
+						index = i;
+					}
+				}
+			}
+		}
+			
+		return powerUps[index];
+	}
+    
+    private Character findNearestPlayer() {
+		double minDis = Double.MAX_VALUE;
+		int index = 0;
+		for (int i = 0; i < players.length; i++) {
+			double tempDis = calcDistance(players[i].getLocation(),getLocation());
+			if( tempDis<minDis ) {
+				minDis = tempDis;
+				index = i;
+			}	
+		}
+		return players[index];
+	}
+    
+    private void moveTo(Character player) {
+    	
+    	Point2D playerLoc = player.getLocation();
+    	double distance = calcDistance(getLocation(),playerLoc );
+    	
+    	while( (int) distance - 1 > getWidth() ) {
+    		
+    		if( (getLocation().getX() - playerLoc.getX()) > 0 )
+    			setLocation(new Point2D( getLocation().getX()-getMovementSpeed(), getLocation().getY()) );
+    		else if( (getLocation().getX() - playerLoc.getX()) < 0 )
+    			setLocation(new Point2D( getLocation().getX()+getMovementSpeed(), getLocation().getY()) );
+    		
+    		if( (getLocation().getY() - playerLoc.getY()) > 0 )
+    			setLocation(new Point2D( getLocation().getX(), getLocation().getY()-getMovementSpeed()) );
+
+    		else if( (getLocation().getY() - playerLoc.getY()) < 0 )
+        			setLocation(new Point2D( getLocation().getX(), getLocation().getY()+getMovementSpeed()) );
+    		
+    		distance = calcDistance(getLocation(), playerLoc);
+    	}
+    }
+    
+    private void moveTo(PowerUp powerup) {
+    
+    	Point2D powerupLoc = powerup.getLocation();
+    	double distance = calcDistance(getLocation(),powerupLoc );
+    	
+    	while( (int) distance - 1 > getWidth() ) {
+    		
+    		if( (getLocation().getX() - powerupLoc.getX()) > 0 )
+    			setLocation(new Point2D( getLocation().getX()-getMovementSpeed(), getLocation().getY()) );
+    		else if( (getLocation().getX() - powerupLoc.getX()) < 0 )
+    			setLocation(new Point2D( getLocation().getX()+getMovementSpeed(), getLocation().getY()) );
+    		
+    		if( (getLocation().getY() - powerupLoc.getY()) > 0 )
+    			setLocation(new Point2D( getLocation().getX(), getLocation().getY()-getMovementSpeed()) );
+
+    		else if( (getLocation().getY() - powerupLoc.getY()) < 0 )
+        			setLocation(new Point2D( getLocation().getX(), getLocation().getY()+getMovementSpeed()) );
+    		
+    		distance = calcDistance(getLocation(), powerupLoc);
+    	}
+    }
+    
+    private double calcDistance(Point2D x, Point2D y) {
+    	return x.distance(y);
+    }
+    
+    @Override
+    public void update() {
+
+    }
 
 
 }
