00f1bce090e8210fa8a4a4dafed5e1eb1f126c9d
“kxf672” <kxf672@student.bham.ac.uk>
2019-02-01 16:46:43 +0000

    added code for broadcasting

--- src/networking/client/MultiCastClient.java
+++ src/networking/client/MultiCastClient.java
@@ -4,47 +4,63 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
+import java.net.UnknownHostException;
+
+import networking.Constants;
 
 //import networking.Constants;
 
 //import networking.Packets.Packet;
 
-public class MultiCastClient  {
+public class MultiCastClient extends ClientNetwork{
+
+	private InetAddress groupAddress;
+	private MulticastSocket socket;
+
+	public MultiCastClient() {
+
+		/*
+		 * To become a member of the "Constants.SERVER_ADDRESS" group, the client calls
+		 * the MulticastSocket's joinGroup method with the InetAddress that identifies
+		 * the group. Now, the client is set up to receive DatagramPackets destined for
+		 * the port and group specified.
+		 */
+		InetAddress group;
+		try {
+			group = InetAddress.getByName(Constants.SERVER_ADDRESS);
+			socket = new MulticastSocket(Constants.SERVER_LISTENING_PORT);
+			this.socket.joinGroup(group);
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+	}
 	
-	public static void main(String[] args) throws IOException {
+	public void run() {
 		
 		
+		byte[] data = new byte[16];
+		DatagramPacket packet;
 
-			
-				/*To become a member of the "Constants.SERVER_ADDRESS" group, the client calls the MulticastSocket's joinGroup method
-				 *  with 
-				 * the InetAddress that identifies the group. 
-				 * Now, the client is set up to receive DatagramPackets destined for the port and group specified. 
-				  */
-				MulticastSocket socket = new MulticastSocket(8888);
-				InetAddress group = InetAddress.getByName("224.0.0.8");
-				socket.joinGroup(group);
+		while (true) {
 
-				byte[] data = new byte[16];
-				DatagramPacket packet;
-				
-				 while(true) {
+			byte[] buf = new byte[256];
+			packet = new DatagramPacket(buf, buf.length);
+			try {
+				socket.receive(packet);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 
-				    byte[] buf = new byte[256];
-			            packet = new DatagramPacket(buf, buf.length);
-			            socket.receive(packet);
+		
 
-				 String received = new String(packet.getData(), 0, packet.getLength());
-		         System.out.println("Quote of the Moment: " + received);
-
-				}
-				
-				/*socket.leaveGroup(group);
-				socket.close();*/
-	
-}
-	
-
-	
+	}
 
 }
