e9624f4b5eb8e839723a258e17d9b1bf0e7901ca
kxr758 <kxr758@student.bham.ac.uk>
2019-02-04 11:59:00 +0000

    Add cooldowns

--- src/entities/CooldownValues.java
+++ src/entities/CooldownValues.java
@@ -5,4 +5,11 @@ public class CooldownValues {
     public static int speedBoostID = 1;
     public static int speedBoostDuration = 4 ;
     public static int lightAttackID = 2;
+    public static float lightAttackCD=1.2f;
+    public static int heavyAttackID = 3;
+    public static float heavyAttackCD = 2.5f;
+    public static int shieldDurID = 4;
+    public static int shieldDuration = 3;
+    public static int shieldID = 5;
+    public static float shieldCD = 2.1f;
 }
