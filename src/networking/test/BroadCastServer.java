ba1a8ce1aa8b7e792383407e676b2b3a6c5bc3f6
“kxf672” <kxf672@student.bham.ac.uk>
2019-02-11 11:35:27 +0000

    deleted unused classes

--- src/networking/test/BroadCastServer.java
+++
@@ -1,9 +0,0 @@
-package networking.test;
-
-import networking.server.MultiCastServerThread;
-
-public class BroadCastServer {
-	public static void main(String[] args) {
-		new MultiCastServerThread().start();
-	}
-}
