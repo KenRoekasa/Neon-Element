0347fcac2fae3dc5972236b34c6af43199b4053f
Jordan Mussi <jxm1099@cs.bham.ac.uk>
2019-02-11 12:37:51 +0000

    Rename

--- src/networking/test/ManualTestServer.java
+++ src/networking/test/ManualTestServer.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 import client.ClientGameState;
 import client.GameStateGenerator;
-import entities.BasicEnemy;
+import entities.Enemy;
 import entities.Player;
 import networking.server.ServerNetwork;
 import server.ServerGameState;
@@ -12,7 +12,7 @@ import server.ServerGameState;
 public class ManualTestServer {
     public static void main(String[] args) {
         ClientGameState clientGameState = GameStateGenerator.createDemoGamestate();
-        ServerGameState gameState = new ServerGameState(new ArrayList<Player>(), new ArrayList<BasicEnemy>(), clientGameState.getMap(), clientGameState.getObjects());
+        ServerGameState gameState = new ServerGameState(new ArrayList<Player>(), new ArrayList<Enemy>(), clientGameState.getMap(), clientGameState.getObjects());
         ServerNetwork net = new ServerNetwork(gameState);
         net.start();
 
