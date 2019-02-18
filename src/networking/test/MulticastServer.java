c75a396b190029fb3fd4608f66636fad3ab7b772
“kxf672” <kxf672@student.bham.ac.uk>
2019-02-18 12:20:47 +0000

    new broadcasting framework

---
+++ src/networking/test/MulticastServer.java
@@ -0,0 +1,7 @@
+package networking.test;
+
+public class MulticastServer {
+	public static void main(String[] args) throws java.io.IOException {
+       // new MulticastServerThread().start();
+    }
+}
