e2615091c640c14f99e9337213ec0e2daed0d668
Tom Wiley <tomos.g.wiley@gmail.com>
2019-02-18 12:09:34 +0000

    Removed duplicate font assignment

--- src/userInterface/Menu.java
+++ src/userInterface/Menu.java
@@ -18,9 +18,6 @@ public class Menu extends Application {
     public void start(Stage primaryStage) throws Exception{
         //TODO do we own the rights to this font? i am rather sceptical
         Font fontLoader = Font.loadFont(getClass().getResourceAsStream("../resources/fonts/Super Mario Bros.ttf"), 14);
-
-
-        Font fontLoader = Font.loadFont(getClass().getResourceAsStream("../resources/fonts/Super Mario Bros.ttf"), 14);
         System.out.println(fontLoader.getName());
 
         FXMLLoader loader = new FXMLLoader(getClass().getResource("menu.fxml"));
