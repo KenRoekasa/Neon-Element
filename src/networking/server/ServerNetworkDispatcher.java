5b16f4fc3d65369cb890c5fa8d0e3420a65aa879
Jordan Mussi <jxm1099@cs.bham.ac.uk>
2019-02-21 17:16:41 +0000

    Broadcast log statement

--- src/networking/server/ServerNetworkDispatcher.java
+++ src/networking/server/ServerNetworkDispatcher.java
@@ -145,7 +145,7 @@ public class ServerNetworkDispatcher extends NetworkDispatcher {
            
             for (PlayerConnection conn : this.connections) {
                 DatagramPacket datagram = new DatagramPacket(data, data.length, conn.getIpAddress(), conn.getPort());
-                System.out.println("" + packet.getIpAddress() + ":" + packet.getPort() + " <-- " + packet.getType());
+                System.out.println("<==" + packet.getType());
 
                 try {
                     this.socket.send(datagram);
