2dfd675f5ff290ac75b24a085f7838f31101b6aa
yxt625 <yxt625@cs.bham.ac.uk>
2019-02-22 12:32:51 +0000

    fixes with hard level ai and printing

--- src/engine/ai/AiController.java
+++ src/engine/ai/AiController.java
@@ -67,7 +67,7 @@ public class AiController {
 		
 		public void startEasyAi() {
 			//aiType = AiType.EASY;
-			System.out.println("started easy ai\n\n");
+			System.out.println("started ai\n difficulty: Easy\n\n");
 			
 			Thread t = new Thread(new Runnable() {
 				
@@ -114,7 +114,7 @@ public class AiController {
 
 		public void startNormalAi() {
 			//aiType = AiType.NORMAL;
-			System.out.println("started normal ai\n\n");
+			System.out.println("started ai\n difficulty: Normal\n\n");
 			Thread t = new Thread(new Runnable() {
 
 				@Override
@@ -130,7 +130,6 @@ public class AiController {
 						else
 							aiPlayer.delay(calc.DELAY_TIME);
 						
-						actions.changeToBefittingElement();
 						normalAIExecuteAction();
 						
 						if (aiPlayer.getHealth() <= 0) {
@@ -148,31 +147,27 @@ public class AiController {
 		
 		public void startHardAi() {
 			//aiType = AiType.HARD;
-			System.out.println("started hard ai\n\n");
+			System.out.println("started ai\n difficulty: Hard\n\n");
 			Thread t = new Thread(new Runnable() {
 
 				@Override
 				public void run() {
 					boolean bool = true;
 					while (bool) {
+						
 						AiFSM.hardAiFetchAction(aiPlayer, aiCon, calc);
-						//System.out.println("health "+aiPlayer.getHealth());
 						
 						if(getActiveState().equals(AiStates.ESCAPE))
 							aiPlayer.delay((calc.DELAY_TIME/2)+(calc.DELAY_TIME/4));
 						else
 							aiPlayer.delay(calc.DELAY_TIME);
 						
-						actions.changeToBefittingElement();
-						easyAIExecuteAction();
-						//delay to limit speed 
+						hardAIExecuteAction();
 						
 						if (aiPlayer.getHealth() <= 0) {
 							aiPlayer.respawn(map.getWidth(),map.getHeight());
 						}
-						
-//						if(timer.off)
-//							bool = false;				
+								
 					}
 				
 				}
@@ -220,6 +215,46 @@ public class AiController {
 		}
 		
 		private void normalAIExecuteAction() {
+			actions.changeToBefittingElement();
+			if(!activeState.equals(AiStates.WANDER))
+				wandering = false;
+			switch (activeState) {
+			case ATTACK:
+				aiPlayer.unShield();
+				attack();
+				break;
+			case AGGRESSIVE_ATTACK:
+				aiPlayer.unShield();
+				aggressiveAttack();
+				break;
+			case FIND_HEALTH:
+				aiPlayer.shield();
+				findHealth();
+				break;
+			case FIND_DAMAGE:
+				aiPlayer.shield();
+				findDamage();
+				break;
+			case FIND_SPEED:
+				aiPlayer.shield();
+				findSpeed();
+				break;
+			case ESCAPE:
+				aiPlayer.shield();
+				escape();
+				break;
+			case WANDER:
+				aiPlayer.unShield();
+				startWandering();
+			case IDLE:
+				break;
+			default:
+				break;
+			}
+		}
+		
+		private void hardAIExecuteAction() {
+			actions.changeToBefittingElement();
 			if(!activeState.equals(AiStates.WANDER))
 				wandering = false;
 			switch (activeState) {
@@ -266,25 +301,25 @@ public class AiController {
 			actions.wander();
 		}
 		
-		public void findSpeed() {
+		private void findSpeed() {
 			int index = calc.getNearestPowerUp(PowerUpType.SPEED);
 			if (index != -1)
 				actions.moveTo(index, calc.getPowerups().get(index).getLocation());
 		}
 
-		public void findDamage() {
+		private void findDamage() {
 			int index = calc.getNearestPowerUp(PowerUpType.DAMAGE);
 			if (index != -1)
 				actions.moveTo(index, calc.getPowerups().get(index).getLocation());
 		}
 
-		public void findHealth() {
+		private void findHealth() {
 			int index = calc.getNearestPowerUp(PowerUpType.HEAL);
 			if (index != -1)
 				actions.moveTo(index, calc.getPowerups().get(index).getLocation());
 		}
 
-		public void aggressiveAttack() {
+		private void aggressiveAttack() {
 			Player player = calc.getNearestPlayer();
 			actions.moveTo(player);
 			if (calc.inAttackDistance(player) && player.getHealth()>0) {
@@ -293,12 +328,12 @@ public class AiController {
 			}
 		}
 		
-		public void escape() {
+		private void escape() {
 			Player player = calc.getNearestPlayer();
 			actions.moveAway(player);
 		}	
 		
-		public void attack() {
+		private void attack() {
 			Player player = calc.getNearestPlayer();
 			actions.moveTo(player);
 
