1179b3c1834013f5ae5739be1f8eb12574e38d8f
Tom Wiley <tomos.g.wiley@gmail.com>
2019-02-24 16:33:29 +0000

    Changed to using audioclips allowing instant playback for spot effects

--- src/client/audiomanager/AudioManager.java
+++ src/client/audiomanager/AudioManager.java
@@ -1,40 +1,37 @@
 package client.audiomanager;
 
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.AudioClip;
 
 import java.io.File;
 import java.util.HashMap;
 
 public class AudioManager {
     private static double volume;
-    private HashMap<Sound, MediaPlayer> soundLibray;
+    private HashMap<Sound, AudioClip> effects;
 
     public AudioManager(){
-        volume = 0;
-        soundLibray = new HashMap<>();
+        volume = 100;
+        effects = new HashMap<>();
 
         for(Sound sound: Sound.values()){
             String location = sound.getPath();
 
-            Media audioFile = new Media(new File(location).toURI().toString());
-            MediaPlayer mp = new MediaPlayer(audioFile);
+            System.out.println(location);
+            AudioClip audioClip = new AudioClip(new File(location).toURI().toString());
 
-            soundLibray.put(sound, mp);
+            effects.put(sound, audioClip);
         }
 
     }
 
     public void playSound(Sound s){
-        soundLibray.get(s).setVolume(volume);
-        soundLibray.get(s).play();
-        soundLibray.get(s).stop();
+        effects.get(s).play(volume);
+
     }
 
     public double getVolume() {
         return volume;
     }
-
     public static void setVolume(double volume) {
         AudioManager.volume = volume;
     }
