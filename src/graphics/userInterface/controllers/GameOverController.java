70e0737eeafe5a7527b7bad36c02ed54a216a61d
Yanjie <yxs793@student.bham.ac.uk>
2019-02-26 23:27:00 +0000

    Resolved pause background not transparent

--- src/graphics/userInterface/controllers/GameOverController.java
+++ src/graphics/userInterface/controllers/GameOverController.java
@@ -1,18 +1,29 @@
 package graphics.userInterface.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class GameOverController extends UIController{
+    @FXML
+    Label back;
 
     public void handleQuitBtn(ActionEvent actionEvent) {
         String fxmlPath = "../fxmls/menu_new.fxml";
         String stageTitle = "Menu";
         String fileException ="Menu";
         FxmlLoader loader = new FxmlLoader(fxmlPath,stage,stageTitle,fileException);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        back.setTextFill(outline);
     }
 }
