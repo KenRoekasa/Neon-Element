531624caf64af681d5edaf0b7f0b597ba4813dc4
kxr758 <kxr758@student.bham.ac.uk>
2019-03-04 12:17:35 +0000

    Add map class

---
+++ src/Map.java
@@ -0,0 +1,19 @@
+import engine.entities.Wall;
+import javafx.scene.shape.Rectangle;
+
+import java.util.ArrayList;
+
+public class Map {
+    Rectangle ground;
+    ArrayList<Wall> walls;
+
+
+
+    public Map(Rectangle ground, ArrayList<Wall> walls) {
+        this.ground = ground;
+        this.walls = walls;
+    }
+    
+
+
+}
