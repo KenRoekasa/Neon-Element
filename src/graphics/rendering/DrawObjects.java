30abf5eeaaf46e7679f184af64870c3bd48b9eab
Tom Wiley <tomos.g.wiley@gmail.com>
2019-03-08 15:39:01 +0000

    Added basic walls
    Think this needs some work

--- src/graphics/rendering/DrawObjects.java
+++ src/graphics/rendering/DrawObjects.java
@@ -103,6 +103,7 @@ class DrawObjects {
     public static void drawObstacles(GraphicsContext gc, Rectangle stageSize, PhysicsObject obstacle, Player player) {
 
         Point2D relativeLocation = getRelativeLocation(stageSize, obstacle , player.getLocation());
+        //relativeLocation = relativeLocation.add(-obstacle.getWidth() / 2f, -obstacle.getHeight() / 2f);
 
 
         gc.save();
