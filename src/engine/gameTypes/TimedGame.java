4fa681dccfc2640fd3a667828b64a76844bac31a
kxr758 <kxr758@student.bham.ac.uk>
2019-03-13 09:43:10 +0000

    Add javadocs to Regicide class

--- src/engine/gameTypes/TimedGame.java
+++ src/engine/gameTypes/TimedGame.java
@@ -1,10 +1,16 @@
 package engine.gameTypes;
 
+/**
+ * A gamemode when the time limit is reach the game ends
+ */
 public class TimedGame extends GameType {
+    /**
+     * How long the game last
+     */
     private long duration;
 
 
-    /**
+    /** Constructor
      * @param duration Duration of how long the match will last in milliseconds
      */
     public TimedGame(long duration) {
