e562f9d79aa1472aabe7831275299608ae312ddc
kxr758 <kxr758@student.bham.ac.uk>
2019-03-13 09:39:37 +0000

    Add javadocs to HillGame class

--- src/engine/gameTypes/HillGame.java
+++ src/engine/gameTypes/HillGame.java
@@ -2,10 +2,27 @@ package engine.gameTypes;
 
 import javafx.scene.shape.Circle;
 
+/**
+ * A Gamemode where there's circular area a player must stay within so that they earn points
+ * win condition stay in the hill as long as possible
+ */
 public class HillGame extends GameType {
+    /**
+     * The circular area where points are earned
+     */
     private Circle hill;
+
+    /**
+     * The score you need to win
+     */
     private float scoreNeeded;
 
+    /**
+     * Constructor
+     *
+     * @param hill        The circular are where points are earned
+     * @param scoreNeeded the scored needed to win/end the game
+     */
     public HillGame(Circle hill, float scoreNeeded) {
         super(Type.Hill);
         this.hill = hill;
