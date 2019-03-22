0e443a6ae88ef5fd7c2ddb235a8620180fbdee53
Jordan Mussi <jxm1099@cs.bham.ac.uk>
2019-03-22 15:39:38 +0000

    Launching sever in new process

--- src/graphics/userInterface/controllers/HostController.java
+++ src/graphics/userInterface/controllers/HostController.java
@@ -4,13 +4,15 @@ import client.GameClient;
 
 import engine.model.generator.GameStateGenerator;
 import client.ClientGameState;
-import server.GameServer;
 import server.ServerGameState;
-import server.ServerGameStateGenerator;
 import javafx.event.ActionEvent;
 import javafx.scene.Scene;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
 
 //For local_setup scene
 public class HostController extends UIController{
@@ -25,11 +27,30 @@ public class HostController extends UIController{
         // create game rules
         // todo make this configurable
         gameState = GameStateGenerator.createEmptyState();
-        serverState = ServerGameStateGenerator.createEmptyState();
         try {
             // Create server
-            GameServer server = new GameServer(serverState);
-            server.start();
+            new Thread(new Runnable() {
+                public void run() {
+                    try {
+                        // create the custom class loader
+                        ClassLoader cl = new URLClassLoader(new URL[0]);
+
+                        // load the class
+                        Class<?> classToLoad = cl.loadClass("networking.test.ManualTestServer");
+
+                        String[] args = new String[0];
+
+                        // get the main method
+                        Method main = classToLoad.getMethod("main", args.getClass());
+
+                        // and invoke it
+                        main.invoke(null, (Object) args);
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
 
             String addr = "localhost";
             GameClient gameBoard = new GameClient(stage, gameState, addr, audioManager);
